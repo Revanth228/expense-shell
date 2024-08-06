890e6fc6ffbeb77d2d2e797a195b94476adc0daf
Revanth228 <devana.reddyrevanth@gmail.com>
2024-08-06 14:10:04 -0500

    commit mysql

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter mysql password"
+read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -40,11 +42,11 @@ VALIDATE $? "Starting mysql server"
 
 #below code will be useful for idempotent nature
 
-mysql -h db.daws78s-rev.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.daws78s-rev.online -uroot -p1${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 
 if [ $? -ne 0 ]
 then
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 VALIDATE $? "MySQL Root password Setup"
 else
 echo -e "Root password is alread set ..... $Y Skipping $N"
